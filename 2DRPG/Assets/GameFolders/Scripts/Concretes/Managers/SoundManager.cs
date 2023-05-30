d6451f79713195489538f6d8f65e0dd64d6c4bb4
BasarEkinci <basarekinci02@outlook.com>
2023-05-30 15:34:28 +0300

    Update

---
+++ 2DRPG/Assets/GameFolders/Scripts/Concretes/Managers/SoundManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TDRPG.Managers
+{
+    public class SoundManager : MonoBehaviour
+    {
+        
+    }    
+}
+
+
